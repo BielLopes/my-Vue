1024148306d29be0a08814d62bb5fa580d266e96
Gabriel Lopes Machado <gabriel.galoma@gmail.com>
2019-08-08 20:47:20 -0300

    Progresso

---
+++ Projects/blog/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue'
+
+export default {
+  components:{
+    'add-blog': addBlog
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  body{
+    margin: 0;
+    font-family: 'Nunito SemiBold'
+  }
+</style>
